59820c362be3a2b5c966252aaf8b77d1d2206f98
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2024-06-21 17:20:14 +0100

    Add script to deploy into chosen namespace

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+set -e
+
+echo "==================="
+echo Deploy a maintenance pod...
+echo "==================="
+echo "Which Kubernetes cluster (ACP) do you want to deploy into?"
+read KUBE_CONTEXT
+echo "Which namespace are you deploying to in that cluster?"
+read NAMESPACE
+echo What is your name?
+read name
+echo "==================="
+echo Thank you $name, deploying now.
+ 
+kubectl --context=$KUBE_CONTEXT -n $NAMESPACE apply -f maintenance-deployment.yml
+
+echo "==================="
+echo Hi $name, deployment is complete. You can exec to the container now.
+
